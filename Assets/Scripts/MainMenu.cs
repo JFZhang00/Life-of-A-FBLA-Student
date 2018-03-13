6f8ab344affee7699d1c2fd1b88ec9d6a567b660
Jason Zhang <jasonzhanger@gmail.com>
2018-03-12 22:00:08 -0700

    -Added Main Menu (It Functions) -Added Scenes: MainMenu and Neighborhood

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+
+    public void PlayGame(){
+        Debug.Log("Hello World");
+        SceneManager.LoadScene("Neighborhood");
+
+    }
+
+    public void QuitGame(){
+        Debug.Log("Quitting Game");
+        Application.Quit();
+
+    }
+
+}
