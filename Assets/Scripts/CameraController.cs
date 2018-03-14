596b12b237d1bbdba0eccefbd63b95822abdcd04
Jason Zhang <jasonzhanger@gmail.com>
2018-03-13 20:51:49 -0700

    -Added Animations -Added Player Movement -Added Camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject followTarget;
+    private Vector3 targetPos;
+    public float moveSpeed;
+    private static bool cameraExists;
+
+    // Use this for initialization
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z); //Assign position of target (the GameObject) to targetPos
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime); //Moves from point A to point B with a speed. Lerp(Vector3 A, Vector3 B, Float Speed)
+
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private bool playerMoving;
+    private Vector2 lastMove;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        playerMoving = false;
+
+
+
+        if (moveHorizontal > 0.5f || moveHorizontal < -0.5f)
+        {
+            playerMoving = true;
+            lastMove = new Vector2(moveHorizontal * Time.deltaTime, 0f);
+            rb.velocity = new Vector2(moveHorizontal * speed * Time.deltaTime, rb.velocity.y);
+        }
+        if (moveVertical > 0.5f || moveVertical < -0.5f)
+        {
+            playerMoving = true;
+            lastMove = new Vector2(0f, moveVertical * Time.deltaTime);
+            rb.velocity = new Vector2(rb.velocity.x, moveVertical * speed * Time.deltaTime);
+
+        }
+
+        if (moveHorizontal < 0.5f && moveHorizontal > -0.5f)
+        {
+            rb.velocity = new Vector2(0f, rb.velocity.y);
+
+        }
+        if (moveVertical < 0.5f && moveVertical > -0.5f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+
+        }
+
+
+
+        anim.SetFloat("moveX", moveHorizontal * Time.deltaTime);
+        anim.SetFloat("moveY", moveVertical * Time.deltaTime);
+        anim.SetBool("playerMoving", playerMoving);
+        anim.SetFloat("lastMoveX", lastMove.x * Time.deltaTime);
+        anim.SetFloat("lastMoveY", lastMove.y * Time.deltaTime);
+
+    }
+
 }
